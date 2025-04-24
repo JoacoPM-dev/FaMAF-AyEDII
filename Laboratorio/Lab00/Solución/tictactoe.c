710cc6c8676128de3352cb7f665b6c7038be4284
JoacoPM-dev <joacopm@proton.me>
2025-04-23 23:41:53 -0300

    Update tictactoe.c

--- Laboratorio/Lab00/Solución/tictactoe.c
+++ Laboratorio/Lab00/Solución/tictactoe.c
@@ -16,8 +16,10 @@ void print_board(char board[3][3])
 {
     int cell = 0;
     print_sep(3);
-    for (int row = 0; row < 3; ++row) {
-        for (int column = 0; column < 3; ++column) {
+    for (int row = 0; row < 3; ++row) 
+    {
+        for (int column = 0; column < 3; ++column) 
+        {
             printf("\t | %d: %c ", cell, board[row][column]);
             ++cell;
         }
@@ -32,25 +34,31 @@ char get_winner(char board[3][3])
     char character;
 
     // Check the rows. 
-    for (size_t r = 0; r < 3; r++) {
+    for (size_t r = 0; r < 3; r++) 
+    {
         bool eq_row = true;
         character = board[r][0];
-        for (size_t c = 0; c < 3; c++) {
+        for (size_t c = 0; c < 3; c++) 
+        {
             eq_row = eq_row && character == board[r][c];
         }
-        if (eq_row && character != '-') {
+        if (eq_row && character != '-') 
+        {
             winner = character;
         }
     }
     
     // Check the columns.
-    for (size_t c = 0; c < 3; c++) {
+    for (size_t c = 0; c < 3; c++) 
+    {
         bool eq_col = true;
         character = board[0][c];
-        for (size_t r = 0; r < 3; r++) {
+        for (size_t r = 0; r < 3; r++) 
+        {
             eq_col = eq_col && character == board[r][c];
         } 
-        if (eq_col && character != '-') {
+        if (eq_col && character != '-') 
+        {
             winner = character;
         }
     }
@@ -58,19 +66,23 @@ char get_winner(char board[3][3])
     // Check the diagonals.
     bool eq_dig = true;
     character = board[0][0];
-    for (size_t i = 0; i < 3; i++) {
+    for (size_t i = 0; i < 3; i++) 
+    {
         eq_dig = eq_dig && character == board[i][i]; 
     }
-    if (eq_dig && character != '-') {
+    if (eq_dig && character != '-') 
+    {
         winner = character;
     }      
 
     eq_dig = true;
     character = board[0][2];
-    for (size_t i = 0; i < 3; i++) {
+    for (size_t i = 0; i < 3; i++) 
+    {
         eq_dig = eq_dig && character == board[i][2-i]; 
     }
-    if (eq_dig && character != '-') {
+    if (eq_dig && character != '-') 
+    {
         winner = character;
     }    
 
@@ -80,53 +92,63 @@ char get_winner(char board[3][3])
 bool has_free_cell(char board[3][3])
 {
     bool free_cell = false;
-    for (size_t row = 0; row < 3; row++) {
-        for (size_t col = 0; col < 3; col++) {
-            if (board[row][col] == '-') {
+    for (size_t row = 0; row < 3; row++) 
+    {
+        for (size_t col = 0; col < 3; col++) 
+        {
+            if (board[row][col] == '-') 
+            {
                 free_cell = true;
             }
         }    
     }
+    
     return free_cell;
 }
 
 int main(void)
 {
-    char board[3][3] = {
-        { '-', '-', '-' },
-        { '-', '-', '-' },
-        { '-', '-', '-' }
-    };
+    char board[3][3] = {{ '-', '-', '-' },
+                        { '-', '-', '-' },
+                        { '-', '-', '-' }};
     char turn = 'X';
     char winner = '-';
     int cell = 0;
 
-    while (winner == '-' && has_free_cell(board)) {
+    while (winner == '-' && has_free_cell(board)) 
+    {
         print_board(board);
         printf("\nTurno %c - Elija posición (número del 0 al %d): ", turn, CELL_MAX);
         int scanf_result = scanf("%d", &cell);
-        if (scanf_result <= 0) {
+        if (scanf_result <= 0) 
+        {
             printf("Error al leer un número desde teclado\n");
             exit(EXIT_FAILURE);
         }
-        if (cell >= 0 && cell <= CELL_MAX) {
+        if (cell >= 0 && cell <= CELL_MAX) 
+        {
             int row = cell / 3;
             int colum = cell % 3;
-            if (board[row][colum] == '-') {
+            if (board[row][colum] == '-') 
+            {
                 board[row][colum] = turn;
                 turn = turn == 'X' ? 'O' : 'X';
                 winner = get_winner(board);
-            } else {
+            } else 
+            {
                 printf("\nCelda ocupada!\n");
             }
-        } else {
+        } else 
+        {
             printf("\nCelda inválida!\n");
         }
     }
     print_board(board);
-    if (winner == '-') {
+    if (winner == '-') 
+    {
         printf("Empate!\n");
-    } else {
+    } else 
+    {
         printf("Ganó %c\n", winner);
     }
     
